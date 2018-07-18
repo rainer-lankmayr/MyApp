db611be0f536b754ff864001e0af822043de92f3
rainer-lankmayr <microft1@gmx.at>
2018-07-18 18:04:47 +0200

    Android Test for Main Activity

--- app/src/androidTest/java/com/example/rainer/myapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/rainer/myapp/ExampleInstrumentedTest.java
@@ -1,13 +1,37 @@
 package com.example.rainer.myapp;
 
 import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.support.design.widget.Snackbar;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.*;
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +40,30 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.rainer.myapp", appContext.getPackageName());
+    }
+
+    @Test
+    public void onFabClick_shouldDisplaySnackbar() throws Exception {
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        FloatingActionButton fab = (FloatingActionButton) withId(R.id.fab);
+//        FloatingActionButton fab = (FloatingActionButton) onView(withId(R.id.fab));
+//        FloatingActionButton fab = (FloatingActionButton) onView(allOf(withParent(withId(R.id.fab)));
+//
+//        fab.performClick();
+
+        onView(withId(R.id.fab)).perform(click());
+
+//        // Compare with the text message of snackbar
+//        onView(withText(R.string.snackbar_message))
+//                .check(matches(isDisplayed()));
     }
 }
